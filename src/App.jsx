864c49379bc640284789f63e51524198934d6a9d
Blackenigma <allanvictorcecilia18@gmail.com>
2024-09-21 11:40:26 +0800

    Layout & Print function added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ export default function App() {
     window.print();
   };
   return (
-    <div className="flex items-start justify-center h-screen mt-10">
+    <>
+     <div className="flex items-start justify-center h-screen mt-10">
       <img className="absolute left-10 w-24" 
            src="/logo.png"
            onClick={handlePrint}/>
@@ -25,6 +26,30 @@ export default function App() {
         <h1 className="font-medium underline">PRELIM EXAM</h1>
         <h1 className="text-sm ml-8">(Term)</h1>
       </div>
+      <div className="absolute top-60">
+        <h1 className="font-medium underline">MATH 3 Analytic Geometry</h1>
+        <h1 className="text-sm -ml-3">(Subject Code and Descriptive Title)</h1>
+      </div>
+      <div className="flex absolute top-72">
+      <h1 className="underline">2nd Term, SY 2024-2025</h1>
+      <h1 className="text-sm ml-3 absolute top-5">(Term)</h1>
+      <h1 className="text-sm ml-28 absolute top-5">(SY)</h1>
+      </div>
     </div>
+    <div className="flex absolute top-[350px]">
+    <h1 className="ml-10">Name: __________________________________________</h1>
+    <h1 className="text-sm ml-28 absolute top-5">Last Name</h1>
+    <h1 className="text-sm ml-52 absolute top-5">First Name</h1>
+    <h1 className="text-sm ml-80 absolute top-5">M.I.</h1>
+    </div>
+    <div className="flex absolute top-[350px] left-[425px]">
+    <h1>Yr & Sec: ______________</h1>
+    <h1 className="ml-12">Score: ______________</h1>
+    </div>
+    <div className="flex absolute top-[410px]">
+    <h1 className="ml-10">Instructor: ______________________________________</h1>
+    <h1 className="ml-14">Date: ______________</h1>
+    </div>
+    </>
   );
 }
